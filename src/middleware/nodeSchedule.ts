b9cc5b31df8dfca8b131767a971b0e98e78a160d
sarvadhikeval <kevalranpariya@sarvadhi.com>
2023-08-15 18:14:28 +0530

    schedule

--- src/middleware/nodeSchedule.ts
+++ src/middleware/nodeSchedule.ts
@@ -4,13 +4,14 @@ import { formattedDate } from '../utils/dateFormat';
 import moment from 'moment';
 export default async()=>{
   scheduleJob('*/5 * * * * *',async date=>{
-    const time = moment(new Date()).format('DD-MM-YYYY hh:mm');
-    // const findAuction = await Auction.findOne({ where: {
-    //   time_start: time
-    // }});
-    if(time == '2023-08-15 16:32'){
-        console.log('enter');
-    }else console
-    console.log(findAuction)
+    const time = moment(new Date()).format('YYYY-MM-DD hh:mm');
+    const findAuction = await Auction.findOne({ where: {
+      time_start: time
+    }});
+    // if(time == '2023-08-15 06:09'){
+    //     console.log('enter');
+    // }else console.log('not enter');
+    // console.log(time);
+    console.log(findAuction);
   });
 };
